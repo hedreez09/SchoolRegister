9e060086b9aa747192ab2130cabee6fbe8063da7
Idris.Wonuola <Idris.Wonuola@wemabank.com>
2020-06-09 14:59:39 +0100

    Unit test performed on some endpoints

--- SchoolRegister.Test/StudentServiceTest.cs
+++ SchoolRegister.Test/StudentServiceTest.cs
@@ -6,7 +6,10 @@ using SchoolRegister.DAL.Entities;
 using SchoolRegister.DAL.Interface;
 using SchoolRegister.Domain;
 using SchoolRegister.Domain.ViewModel;
+using System.Collections.Generic;
+using System.Linq;
 using System.Reflection;
+using System.Threading.Tasks;
 
 namespace SchoolRegister.Test
 {
@@ -26,15 +29,17 @@ namespace SchoolRegister.Test
 		{
 			// Arrange
 			var stud = new Student();
-			var studentName = "idris Wonuola";
-			var student = new StudentViewModel
+			var studentName = "idris";
+			var studentName2 = 	"Wonuola";
+			var student = new Student
 			{
 				Id = 1,
-				FullName = studentName,
-				Age = 18,
+				FirstName = studentName,
+				LastName = studentName2,
 				Gender = "Male",
 				Sport = "Tennis",
 				Level = "Basic4",
+				
 			};
 
 			_studentRepositoryMock.Setup(s => s.GetById(student.Id)).Returns(student);
@@ -46,9 +51,75 @@ namespace SchoolRegister.Test
 			Assert.That(studenttoCheck.Gender, Is.EqualTo(student.Gender));
 			Assert.That(studenttoCheck.Level, Is.EqualTo(student.Level));
 			Assert.That(studenttoCheck.Sport, Is.EqualTo(student.Sport));
-
-
 		}
 		
+		[Test]
+		public void GetAllStudentThatExist()
+        {
+
+			//Arrange
+			IList<StudentViewModel> studentViewModel = new List<StudentViewModel>()
+			{
+				new StudentViewModel
+				{
+					Id = 1,
+					FullName = "Adeola Adelakun",
+					Sport = "Tennis",
+					Gender = "Male",
+					Age = 20,
+					Level = "Basic4"
+				},
+
+
+				new StudentViewModel
+				{
+					Id = 2,
+					FullName = "Adeola Adelakun",
+					Sport = "Tennis",
+					Gender = "Male",
+					Age = 20,
+					Level = "Basic4"
+				},
+
+
+				new StudentViewModel
+				{
+					Id = 3,
+					FullName = "AdeTola olalakun",
+					Sport = "Tennis",
+					Gender = "Male",
+					Age = 20,
+					Level = "Basic4"
+				},
+			};
+
+			_studentRepositoryMock.Setup(s => s.GetStudents());
+
+			//Act
+			var expectedStudent = 3;
+
+			Assert.That(expectedStudent, Is.EqualTo(studentViewModel.Count()));
+		}
+
+		[Test]
+		public void AddingNewStudent()
+        {
+			var newStudent = new Student()
+			{
+				FirstName = "Emma",
+				LastName = "Nwankwo",
+				Gender = "male",
+				Sport = "Football",
+				Level = "basic5",
+				//DateOfBirth = GetCurrentAge()
+			};
+
+			//var stud = _studentRepositoryMock.Setup(s => s.AddStudent(newStudent));
+
+			//var stud = new Student(); 
+
+			//Assert.IsTrue(stud);
+
+		}
 	}
 }
